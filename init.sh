9c48392d59ff7bfb2a1623949195e96da50b63cc
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2014-07-25 15:34:09 -0300

    Fix some bugs

--- init.sh
+++ init.sh
@@ -21,7 +21,7 @@ mkdir ./src
 
 echo "Copying files..."
 cp ./template/assets/js/main.js ./assets/js/main.js
-cp ./template/assets/js/vendor/modernizr-2.6.2.min.js ./assets/js/vendor/modernizr-2.6.2.min.js
+cp ./template/assets/js/vendors/modernizr-2.6.2.min.js ./assets/js/vendors/modernizr-2.6.2.min.js
 cp ./template/assets/sass/base/_base.scss ./assets/sass/base/_base.scss
 cp ./template/assets/sass/base/_normalize.scss ./assets/sass/base/_normalize.scss
 cp ./template/assets/sass/helpers/mixins/_backface-visibility.scss ./assets/sass/helpers/mixins/_backface-visibility.scss
