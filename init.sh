1b8d8997ba6b4b37da67441430022a47bba3f5ee
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2014-08-27 11:58:42 -0300

    Add command to initi project with gulp

--- init.sh
+++ init.sh
@@ -6,6 +6,14 @@ rm -rf .gitignore
 cp ./template/.gitignore ./.gitignore
 git init
 
+echo "Initializing the project..."
+sudo npm init
+npm install gulp-jshint --save-dev #Gulp JSHint
+npm install gulp-sass --save-dev #Gulp Sass
+npm install gulp-concat --save-dev #Gulp Concat
+npm install gulp-uglify --save-dev #Gulp Uglify
+npm install gulp-rename --save-dev #Gulp rename
+
 
 # echo "Grunt or Gulp? (Answer with the number)"
 # select runner in "Grunt" "Gulp" "Neither"; do
