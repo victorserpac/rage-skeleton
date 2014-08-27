620b43e8eabf6ad157534599e842e02d38f25c10
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2014-08-27 11:13:37 -0300

    Moving structure for less code in init.sh

--- init.sh
+++ init.sh
@@ -1,70 +1,10 @@
 #!/bin/bash
 
-echo "Initializing structure..."
-mkdir ./assets
-mkdir ./assets/css
-mkdir ./assets/fonts
-mkdir ./assets/images
-mkdir ./assets/js
-mkdir ./assets/js/components
-mkdir ./assets/js/vendors
-mkdir ./assets/sass
-mkdir ./assets/sass/base
-mkdir ./assets/sass/components
-mkdir ./assets/sass/helpers
-mkdir ./assets/sass/helpers/mixins
-mkdir ./assets/sass/helpers/placeholders
-mkdir ./assets/sass/layout
-mkdir ./assets/sass/pages
-mkdir ./assets/sass/vendors
-mkdir ./src
-
-echo "Copying files..."
-# Copy files
-# ============================
-
-# JS
-cp ./template/assets/js/main.js ./assets/js/main.js
-
-# JS Vendors
-cp ./template/assets/js/vendors/modernizr-2.6.2.min.js ./assets/js/vendors/modernizr-2.6.2.min.js
-cp ./template/assets/js/vendors/vs-placeholder.js ./assets/js/vendors/vs-placeholder.js
-
-
-# Sass Base
-cp ./template/assets/sass/base/_base.scss ./assets/sass/base/_base.scss
-cp ./template/assets/sass/base/_reset.scss ./assets/sass/base/_reset.scss
-
-# Sass Mixins
-cp ./template/assets/sass/helpers/mixins/_image-background.scss ./assets/sass/helpers/mixins/_image-background.scss
-cp ./template/assets/sass/helpers/mixins/_img-responsive.scss ./assets/sass/helpers/mixins/_img-responsive.scss
-cp ./template/assets/sass/helpers/mixins/_responsive.scss ./assets/sass/helpers/mixins/_responsive.scss
-cp ./template/assets/sass/helpers/mixins/_sprite-icons.scss ./assets/sass/helpers/mixins/_sprite-icons.scss
-cp ./template/assets/sass/helpers/mixins/_text-emphasis.scss ./assets/sass/helpers/mixins/_text-emphasis.scss
-cp ./template/assets/sass/helpers/mixins/_vendor-prefixes.scss ./assets/sass/helpers/mixins/_vendor-prefixes.scss
-
-# Sass Placeholders
-cp ./template/assets/sass/helpers/placeholders/_center-absolute.scss ./assets/sass/helpers/placeholders/_center-absolute.scss
-cp ./template/assets/sass/helpers/placeholders/_clear.scss ./assets/sass/helpers/placeholders/_clear.scss
-cp ./template/assets/sass/helpers/placeholders/_span-helper.scss ./assets/sass/helpers/placeholders/_span-helper.scss
-
-# Sass Helpers
-cp ./template/assets/sass/helpers/_config.scss ./assets/sass/helpers/_config.scss
-cp ./template/assets/sass/helpers/_mixins.scss ./assets/sass/helpers/_mixins.scss
-cp ./template/assets/sass/helpers/_placeholders.scss ./assets/sass/helpers/_placeholders.scss
-
-# Sass
-cp ./template/assets/sass/README.md ./assets/sass/README.md
-cp ./template/assets/sass/style.scss ./assets/sass/style.scss
-
-# Source
-cp ./template/src/config.rb ./src/config.rb
-cp ./template/src/README.md ./src/README.md
-
-# Root
-cp ./template/.editorconfig ./.editorconfig
+echo "Initializing git..."
+rm -rf .git
+rm -rf .gitignore
 cp ./template/.gitignore ./.gitignore
-cp ./template/index.html ./index.html
+git init
 
 
 # echo "Grunt or Gulp? (Answer with the number)"
@@ -77,7 +17,7 @@ cp ./template/index.html ./index.html
 # done
 
 echo "Removing stuff you don't want..."
-rm -rf .git
+
 rm -rf template
 rm README.md
 rm init.sh
