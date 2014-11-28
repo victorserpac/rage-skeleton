f7f050d25f1238c401dac7460556cea21f58caa6
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2014-11-28 18:07:01 -0200

    Install just with npm install, not each plugin

--- init.sh
+++ init.sh
@@ -5,26 +5,11 @@ echo "============== Initializing git... ================="
 rm -rf .git #'Reset' Git
 git init
 
-echo " ==> Initializing the package.json to Gulp, you can just press Enter for all inputs"
-cd src/
-sudo npm init #inciar projeto npm
-
-
 #Installing modules
 clear
 echo "==> Installing Gulp and dependencies..."
-sudo npm install gulp --save-dev #Gulp
-sudo npm install gulp-jshint --save-dev #Gulp JSHint
-sudo npm install gulp-concat --save-dev #Gulp Concat
-sudo npm install gulp-uglify --save-dev #Gulp Uglify
-sudo npm install gulp-rename --save-dev #Gulp rename
-sudo npm install gulp-compass --save-dev #Gulp Compass
-sudo npm install gulp-livereload --save-dev #Gulp LiveReload
-sudo npm install gulp-autoprefixer --save-dev #Gulp Auto Prefixer
-sudo npm install gulp-combine-media-queries --save-dev #Gulp Combine Media Queries
-sudo npm install gulp-watch --save-dev #Gulp watch
-sudo npm install gulp-image-resize --save-dev #Gulp Image Resize
-sudo npm install gulp-sharp --save-dev
+cd src/
+sudo npm install
 cd ../
 
 
@@ -37,6 +22,7 @@ rm init.sh
 rm ./assets/css/.gitkeep
 rm ./assets/fonts/.gitkeep
 rm ./assets/images/icons/.gitkeep
+rm ./assets/images/src/.gitkeep
 rm ./assets/js/components/.gitkeep
 rm ./assets/js/vendors/.gitkeep
 rm ./assets/sass/components/.gitkeep
